8b8d7f3a979c4b9f7f5cf9f62dd72074077d6f74
youssef el hajj <youssefelhajj@youssefs-MacBook-Air.local>
2025-04-15 17:17:24 +0200

    Initial commit for Swipply project

--- lib/pages/cv.dart
+++ lib/pages/cv.dart
@@ -8,7 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:swipply/constants/themes.dart';
 import 'package:open_file/open_file.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;“‘
 import 'dart:convert';
 
 import 'package:swipply/env.dart';
